10c4ab331985318f8a2bf8e53825aa0f709eaf81
Code Dani <ysldaves@gmail.com>
2026-02-09 17:21:57 -0700

    Java Cs1

---
+++ lab.java
@@ -0,0 +1,9 @@
+public class lab {
+  public static void main (String[] args){
+		int number1 = 17;
+    int number2 = 5;
+    System.out.println(number1);
+    System.out.println(number2);
+    System.out.println(number1 / number2);
+}
+}
