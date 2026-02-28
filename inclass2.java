ac61271d437bfe7312c5bb829a415f889e0f34a9
Code Dani <ysldaves@gmail.com>
2026-02-27 22:01:01 -0700

    done class2

--- inclass2.java
+++ inclass2.java
@@ -1,7 +1,6 @@
-import java.util.Scanner;
-import java.io.FIle;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.util.Scanner;
 
 // Questions to answer!
 // What is the highest monday temperature?
@@ -25,16 +24,21 @@ public class inclass2{
              * perform the counting and calculations
             */
             // YOUR CODE HERE
+            double highestMonday = Double.MIN_VALUE;
+            double lowestFriday = Double.MAX_VALUE;
+
             double mondaySum = 0;
             int mondayCount = 0;
             double  tuesdaySum = 0;
             int tuesdayCount = 0;
-            double wensdaySum = 0;
-            int wensdayCount = 0;
+            double wednesdaySum = 0;
+            int wednesdayCount = 0;
             double thursdaySum = 0; 
-            int = thursdayCount = 0;
-            double = fridaySum = 0;
-            int = fridayCount = 0;
+            int thursdayCount = 0;
+            double  fridaySum = 0;
+            int  fridayCount = 0;
+            int daysAbove70 = 0;
+
             /**
              * TODO
              * Traverse the file using a loop (what type of loop would be best for this situation?)
@@ -43,6 +47,33 @@ public class inclass2{
              * Compute the calculations needed
             */
             // YOUR CODE HERE
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (line.isEmpty()) continue; 
+
+                Scanner lineScanner = new Scanner(line);
+
+                double monday = lineScanner.nextDouble();
+                double tuesday = lineScanner.nextDouble();
+                double wednesday = lineScanner.nextDouble();
+                double thursday = lineScanner.nextDouble();
+                double friday = lineScanner.nextDouble();
+
+                mondaySum += monday; mondayCount++;
+                tuesdaySum += tuesday; tuesdayCount++;
+                wednesdaySum += wednesday; wednesdayCount++;
+                thursdaySum += thursday; thursdayCount++;
+                fridaySum += friday; fridayCount++;
+
+                if (monday > highestMonday) highestMonday = monday;
+                if (friday < lowestFriday) lowestFriday = friday;
+
+                if (monday > 70) daysAbove70++;
+                if (tuesday > 70) daysAbove70++;
+                if (wednesday > 70) daysAbove70++;
+                if (thursday > 70) daysAbove70++;
+                if (friday > 70) daysAbove70++;
+            }
 
 
             /**
@@ -50,16 +81,20 @@ public class inclass2{
              * Fill the FIXMEs in the print statements below so you are able to display your results
              * to the console
             */
-            System.out.println("Highest monday temperature: " + FIXME);
-            System.out.println("Lowest friday temperature: " + FIXME);
-            System.out.println("Days above 70: " + FIXME);
+            System.out.println("Highest Monday temperature: " + highestMonday);
+            System.out.println("Lowest Friday temperature: " + lowestFriday);
+            System.out.println("Days above 70: " + daysAbove70);
 
             /**
              * Print out the avg temperature per day, complete as needed
             */
-            System.out.println("Avg temperature for monday: ");
+            System.out.println("Avg temperature for Monday: " + (mondaySum / mondayCount));
+            System.out.println("Avg temperature for Tuesday: " + (tuesdaySum / tuesdayCount));
+            System.out.println("Avg temperature for Wednesday: " + (wednesdaySum / wednesdayCount));
+            System.out.println("Avg temperature for Thursday: " + (thursdaySum / thursdayCount));
+            System.out.println("Avg temperature for Friday: " + (fridaySum / fridayCount));
 
-            scan.close(); // this line right here just closes the scanner once we are done using it
+            scanner.close(); // this line right here just closes the scanner once we are done using it
 
         } catch(FileNotFoundException e){
             /**
