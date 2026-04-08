1c527033f75efb5a90f11406a1beb732ca8e242e
danipicks <danipicks04@gmail.com>
2026-04-08 17:04:22 -0600

    set it up

---
+++ Lab6.java
@@ -0,0 +1,171 @@
+package org.utep.cs1.labs;
+
+public class Lab6 {
+    /*
+    * Welcome to Lab 5!
+    *
+    * The focus of this lab will be to make you comfortable dealing and working
+    * with 2D arrays.
+    *
+    * Complete all the methods requested below, make sure to test them!
+    * Also, please do not modify the method's signature!
+    * */
+    public static void main(String[] args) {
+        // Make sure to test the methods that you've completed here in main!
+    }
+
+
+    /*
+     * This method returns the sum of the numbers in both diagonals of a
+     * squared 2D array.
+     *
+     * e.g.
+     * 1 2 3
+     * 4 5 6
+     * 7 8 9
+     * The answer would be 1+5+9 (upper left to lower right)  + 3+5+7 (upper right to lower left)
+     * = 30
+     * */
+    public static int sumDiagonals(int[][] arr){
+        //Create your method here
+        return 0;
+    }
+
+
+    /*
+    * This method prints out all the elements in a squared array in spiral order.
+    * Starting in the top left corner and then going right, down, left, up until you reach the middle
+    *
+    * e.g.
+    * 1 2 3
+    * 4 5 6
+    * 7 8 9
+    * output = 1 2 3 6 9 8 7 4 5
+    *
+    * 1 2
+    * 3 4
+    * output = 1 2 4 3
+    * */
+    public static void spiral(int[][] arr){
+        //Create your method here
+    }
+
+    /*
+    * This method returns a new 1D array with the sum of each of the columns in a squared array
+    *
+    * e.g.
+    * 1 2 3
+    * 4 5 6
+    * 7 8 9
+    * output = [12 15 18]
+    *
+    *
+    * 1 2
+    * 3 4
+    * output = [4 6]
+    * */
+    public static int[] sumColumns(int[][] arr){
+        //Create your method here
+        return null;
+    }
+
+    /*
+    * Given a squared 2D array, this method returns the element at the middle position
+    * of the whole array. If there is no exact middle element, then it returns the avg of the elements
+    * that form the middle square
+    *
+    * e.g.
+    * 1 2 3
+    * 4 5 6
+    * 7 8 9
+    * output = 5.0
+    *
+    * 1 2
+    * 3 4
+    * output = 1+2+3+4/4 = 10/4 = 2.5
+    *
+    * */
+    public static double getMiddle(int[][] arr){
+        //Create your method here
+        return 0;
+    }
+
+    /*
+    * Given two 2D arrays, return true if they are exactly the same, false otherwise
+    *
+    * e.g.
+    * 1 2 3    1 2 3
+    * 4 5 6    4 5 6
+    * 7 8 9    7 8 9
+    * output = true
+    *
+    * 1 2      2 3
+    * 3 4      1 4
+    * output = false
+    * */
+    public static boolean arraysAreEqual(int[][] arr1, int[][] arr2){
+        //Create your method here
+        return false;
+    }
+
+    /*
+    * Given a 2D array, and two integers rowAIndex and rowBIndex, swap the elements
+    * in the two rows given, and return the resulting array
+    *
+    * e.g.
+    * rowAIndex = 0, rowBIndex = 2
+    * 1 2 3
+    * 4 5 6
+    * 7 8 9
+    * output =
+    * 7 8 9
+    * 4 5 6
+    * 1 2 3
+    *
+    * rowAIndex = 0, rowBIndex = 1
+    * 1 2
+    * 3 4
+    * output =
+    * 3 4
+    * 1 2
+    * */
+    public static int[][] rowSwap(int[][] arr, int rowAIndex, int rowBIndex){
+        //Create your method here
+        return null;
+    }
+
+    /*
+    * Given the following parameters:
+    * arr = 2D array
+    * startRow = the first row to be kept
+    * startCol = the first column to be kept
+    * endRow = the last row to be kept
+    * endCol = the last column to be kept
+    *
+    * return a smaller 2D array containing the elements in the given range
+    *
+    * e.g.
+    * 1 2 3
+    * 4 5 6
+    * 7 8 9
+    * startRow = 1, startCol = 1, endRow = 2, endCol = 2
+    * output:
+    * 5 6
+    * 8 9
+    *
+    *
+    * 1 2
+    * 3 4
+    * startRow = 0, startCol = 0, endRow = 0, endCol = 1
+    * output:
+    * 1 2
+    * */
+    public static int[][] cropArray(int[][] arr, int startRow, int startCol, int endRow, int endCol){
+        return null;
+    }
+
+
+
+
+
+}
---
+++ twoDarrays/InClassExercise5.java
@@ -0,0 +1,32 @@
+package twoDarrays;
+public class InClassExercise5 {
+	public static void main(String[] args){
+		// test your methods here
+		int[][] arr = {{1, 2, 3}, 
+					   {4, 5, 6}, 
+					   {7, 8, 9}};
+
+		System.out.println(sumDiagonals(arr));
+		System.out.println(sumOuter(arr));
+		System.out.println(sumColumns(arr));
+	}
+
+	// ex 1
+	public static int sumDiagonals(int[][] arr){
+        //Create your method here
+        return 0;
+    }
+
+
+	// ex 2
+	public static int sumOuter(int[][] arr){
+        //Create your method here
+        return 0;
+	}
+
+	// ex 3
+	public static int sumColumns(int[][] arr){
+        //Create your method here
+        return 0;
+	}
+}
